f3e36eb9fb954123fcd61c5d6b30e84a4c6f65bf
Phuc Le <10k.polime.vnd@gmail.com>
2026-04-28 04:49:29 +0700

    chore: vendor protoc binary to ensure consistent compilation in build script

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path()?);
     let protos = ["proto/agent/registry/v1/agent_registry.proto"];
 
     tonic_build::configure()
